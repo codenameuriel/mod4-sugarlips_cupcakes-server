397c327531c5632e908ac8dbb323cfe070876ae1
diana2341 <dianaponce2341@gmail.com>
2020-05-21 17:51:37 -0400

    fixed controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,39 +1,39 @@
-class Api::V1::ProfileController < ApplicationController
+class ProfileController < ApplicationController
 
     def index
-        @profiles=Profile.all 
-        render json:@profiles
+        profiles=Profile.all 
+        render json:profiles
 
     end
 
     def show
-        @profile=Profile.find(params[:id])
-        render json:@profile
+        profile=Profile.find(params[:id])
+        render json:profile
     end
 
     def create
-        @profile=Profile.new(profile_params)
-        if @profile.save
-            render json:@profile 
+        profile=Profile.new(profile_params)
+        if profile.save
+            render json:profile 
         else
             render error:{'error:unable to create profile'},status:400
         end
     end
 
     def update 
-        @profile=Profile.find(params[:id])
-        if @profile 
-            @profile.update(profile_params)
+        profile=Profile.find(params[:id])
+        if profile 
+            profile.update(profile_params)
             render json:{message:'profile succesfilly updates'}, status 200
         else
             render json:{error:'unable to update profile'}, status:400
         end
     end
 
-    def update 
-        @profile=Profile.find(params[:id])
-        if @profile 
-            @profile.destroy
+    def delete 
+        profile=Profile.find(params[:id])
+        if profile 
+            profile.destroy
             render json:{message:'profile succesfilly deleted'}, status 200
         else
             render json:{error:'unable to delete profile'}, status:400
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_one :profile
+    has_many :carts
+    has_many :cupcakes, through: :carts
 end
