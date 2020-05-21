0b1a4f2e5dd8bf783d74776f8d52a26213baf876
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-21 15:59:48 -0400

    add cart controller actions

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,19 @@
 class CartsController < ApplicationController
+
+  def create
+    cart = Cart.create(cart_params)
+    render json: cart
+  end
+
+  def delete
+    cart = Cart.find(params[:id])
+    cart.destroy
+    render json: cart
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:user_id, :cupcake_id)
+  end
 end
